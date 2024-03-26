309858c2a19af814fcad2ada63112fa784f39394
PedroPepeu <pedrosteamgtav@gmail.com>
2024-03-26 20:12:35 -0300

    adding login data add

--- acc/script.js
+++ acc/script.js
@@ -7,6 +7,20 @@ const usr_gender = document.getElementById("usr_gender");
 const usr_state = document.getElementById("usr_state");
 
 function save_acc() {
+    let check_login = 1; // 1 to sucessfull login / 0 to failed login
+
     let name = usr_name.value;
-    console.log(name);
+    if(name.length > 12) {
+        check_login = 0;
+        document.getElementById("name_rtn").innerHTML = "Name is to big!";
+    }
+
+    let email = usr_email.value;
+    let birth = usr_birth.value;
+    let psswd = usr_psswd.value
+    let c_psswd = usr_confirm_psswd.value;
+    let gender = usr_gender.value;
+    let state = usr_gender;
+
+    
 }
