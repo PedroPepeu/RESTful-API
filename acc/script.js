12f7b818fdb51ecd8fb71238aedada5dde8c0acb
PedroPepeu <pedrosteamgtav@gmail.com>
2024-03-26 19:30:24 -0300

    adding configs

---
+++ acc/script.js
@@ -0,0 +1,12 @@
+const usr_name  = document.getElementById("usr_name");
+const usr_email = document.getElementById("usr_email");
+const usr_birth = document.getElementById("usr_birth");
+const usr_psswd = document.getElementById("usr_psswd");
+const usr_confirm_psswd = document.getElementById("usr_confirm_psswd");
+const usr_gender = document.getElementById("usr_gender");
+const usr_state = document.getElementById("usr_state");
+
+const checkInputs = () => {
+    let name = usr_name.value;
+    console.log(name);
+}
